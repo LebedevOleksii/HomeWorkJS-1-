5b5f6aaab51b40c513c1cb855c78e23a67cd238f
Oleksii Lebedev <lebediew.oleksii@gmail.com>
2018-11-21 15:07:52 +0200

    fixed task 3

--- HomeWork.js
+++ HomeWork.js
@@ -66,56 +66,55 @@
 
 // <!-----------------------TASK 3  ------------------------------>
 
-// const users = {
-//   1: {
-//     name: 'Roman',
-//     lastName: 'Lurov',
-//     otherData: {
-//       role: 'student'
-//     }
-//   },
-
-//   2: {
-//     name: 'Ivan',
-//     lastName: 'Bondarenko',
-//     otherData: {
-//       role: 'student'
-//     }
-//   },
-
-//   3: {
-//     name: 'Roman',
-//     lastName: 'Lurov',
-//     otherData: {
-//       role: 'student'
-//     }
-//   },
-  
-//   4: {
-//     name: 'Oleg',
-//     lastName: 'Murko',
-//     otherData: {
-//       role: 'student'
-//     }
-//   },
-
-//   5: {
-//     name: 'Kurz',
-//     lastName: 'Smith',
-//     otherData: {
-//       role: 'student'
-//     }
-//   },
-// };
-
-// Object.keys(users).forEach(function(key) {
-//   const user = users[key];
-//   if(user) {
-//      console.log(key + ') name: ' + user.name);
-//      console.log('last name: ' + user.lastName);
-//      console.log('role: ' + user.otherData.role)
-//   }
-// })
+// try {
+//     const users = {
+//       1: {
+//         name: 'Roman',
+//         lastName: 'Lurov',
+//         otherData: {
+//           role: 'student'
+//         }
+//       },
+    
+//       2: {
+//         name: 'Ivan',
+//         lastName: 'Bondarenko',
+//         otherData: {
+//           role: 'student'
+//         }
+//       },
+    
+//       3: {
+//         name: 'Roman',
+//         lastName: 'Lurov',
+//         otherData: {
+//           role: 'student'
+//         }
+//       },
+      
+//       4: {
+//         name: 'Oleg',
+//         lastName: 'Murko',
+//         otherData: {
+//           role: 'student'
+//         }
+//       },
+    
+//       5: {
+//         name: 'Kurz',
+//         lastName: 'Smith'
+//       }
+//     };
+    
+//     Object.keys(users).forEach(function(key) {
+//       const user = users[key];
+//          console.log(key + ") name:"  + user.name);
+//          console.log("last name:"  + user.lastName);
+//          console.log("role:"  + user.otherData.role)
+//     })
+//     } catch(e) { 
+//       console.log("If there is no property, please make sure all data is entered correctly");
+// }
 
 // <!-----------------------TASK 4  ------------------------------>
 
